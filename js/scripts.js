d9c40cc7b72a53416eead3a5187175053c8b3c77
gbrough <gbrough@gmail.com>
2021-06-01 10:37:26 -0700

    add liters to gallons function

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,9 @@ function gallonsToLiters(gallon){
   liters = (gallon * 3.785);
   return liters;
 }
+function litersToGallons(liters){
+  gallons = (liters /  3.785);
+  return gallons;
+}
+litersToGallons(1)
 gallonsToLiters(1)
